fe06159e257c1acfb363a0047c0f0000fc58868c
mannqo <2300765372@qq.com>
2021-12-09 19:48:51 +0800

    login unfinished

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,18 +1,21 @@
 import React, { memo, useEffect } from 'react';
 import styled from 'styled-components';
 import { message, Spin } from 'antd';
-import { getIdentity, getCode } from '../../services/login';
+import { getIdentity } from '../../services/login';
 
 export default memo(function Login() {
     useEffect(async () => {
+        const AppId = 'wxe312cdb484d8af13';
+        const local = window.location.href;
+        window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + AppId + "&redirect_uri=" + local + "&response_type='code'&scope='snsapi_base'#wechat_redirect";
+        console.log(window.location.href);
         try {
-            const code = await getCode('/');
-            alert(code);
-            const identity = await getIdentity(code);
-            alert(identity)
+            // const identity = await getIdentity(code);
+            // console.log(identity);  // 不知道返回信息长啥样
+            // 返回正确的code之后跳转到/main页面就好
 
         } catch (err) {
-            message.error('请再微信客户端打开此链接');
+            message.error('出错了');
         }
 
     }, [])
@@ -24,7 +27,7 @@ export default memo(function Login() {
 })
 
 const SpinWrapper = styled.div`
-    text-align: center;
-    margin-top: 200px;
-`
+        text-align: center;
+        margin-top: 200px;
+ `
 
--- src/services/login.js
+++ src/services/login.js
@@ -1,21 +1,7 @@
-import request from "./axios";
-
-
-// 这里scope需要公众号有权限啥的em
-export function getCode(url) {
-    return request({
-        url: `https://open.weixin.qq.com/connect/oauth2/authorize?
-        appid=wxe312cdb484d8af13
-        redirect_url=`+ url + `&
-        response_type='code'&
-        scope='snsapi_base'&
-        #wechat_redirect
-        `,
-    })
-}
+import request from "./axios"; 
 
 export function getIdentity(code) {
     return request({
-        url: "/user/login" + code,
+        url: "/user/login?" + code,
     })
 }
